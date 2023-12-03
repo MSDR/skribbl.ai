aec1e70ab9442112e62100880f608b19a375feac
Almog Cohen <me@almogcohen.com>
2023-12-02 19:01:13 -0500

    Limit image color palette

--- ImgUtils/sketch.py
+++ ImgUtils/sketch.py
@@ -21,12 +21,31 @@ def split_sketch_into_components(image: Image):
     img_gray = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2GRAY)
 
     # Remove background from simple mask 
-    _, thres = cv2.threshold(img_gray, 165, 255, cv2.THRESH_BINARY_INV)
+    _, thres = cv2.threshold(img_gray, 190, 255, cv2.THRESH_BINARY_INV)
     mask = thres == 0
     replacement_value = np.array([255, 255, 255, 0], dtype=np.uint8)
     img_cv[mask] = replacement_value
 
+    # Restrict to specific colors
+    color_palette = np.array([[0, 0, 0, 1], [255, 10, 30, 1], [30, 255, 90, 1], [30, 50, 255, 1], [120, 60, 30, 1], [240, 240, 0, 1], [255, 255, 255, 1]])
+
+    # Flatten the image into a 2D array of pixels
+    pixels = img_cv.reshape(-1, 4)
+
+    # Initialize an array to store the quantized colors
+    quantized_colors = np.zeros_like(pixels)
+
+    # For each pixel, find the nearest color in the predefined palette
+    for i in range(len(pixels)):
+        pixel = pixels[i]
+        distances = np.linalg.norm(color_palette - pixel, axis=1)
+        nearest_color_index = np.argmin(distances)
+        quantized_colors[i] = color_palette[nearest_color_index]
+    
+    img_cv = quantized_colors.reshape(512, 512, 4)
+
     # Find contours, filter and sort them
+    img_gray = cv2.cvtColor(img_cv, cv2.COLOR_RGB2GRAY)
     _, img_bw = cv2.threshold(img_gray, 127, 255, cv2.THRESH_BINARY)
     contours, _ = cv2.findContours(img_bw, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     contours = [cnt for cnt in contours if 
@@ -46,9 +65,6 @@ def split_sketch_into_components(image: Image):
 
     # Incase we miss anything, pass the original image in as the last one
     imgs.append(img_cv)
-
-    # To see results
-    # display_images(imgs)
 
     # Convert all of these to PIL images with RGBA values
     return [Image.fromarray(cv2.cvtColor(img, cv2.COLOR_BGRA2RGBA)) for img in imgs] 
--- ImgUtils/test.py
+++ ImgUtils/test.py
@@ -3,5 +3,11 @@
 from PIL import Image
 from sketch import *
 
-image = Image.open('test_imgs/tree.png')
+image = Image.open('test_imgs/pizza.png')
 imgs = split_sketch_into_components(image)
+
+# To see results
+images = []
+for img in imgs:
+    images.append(cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGRA))
+display_images(images)
