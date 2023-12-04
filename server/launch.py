fe4dbf16ef30bcf51371e17da3e495310c6928d0
Almog Cohen <me@almogcohen.com>
2023-12-04 18:41:23 -0500

    Added server test_mode and prompt_to_sketch support

--- server/launch.py
+++ server/launch.py
@@ -1,12 +1,23 @@
+import base64
+from io import BytesIO
 from flask import Flask, request, jsonify
 from werkzeug.utils import secure_filename
 from PIL import Image
 from stg import SketchToGuess
+from pts import PromptToSketch
 import os
+import sys
+
+TEST_MODE = 'test_mode' in sys.argv
 
 app = Flask(__name__)
-print('Preparing SketchToGuess Model')
-stg = SketchToGuess()
+
+if not TEST_MODE:
+    print('Preparing SketchToGuess Model')
+    stg = SketchToGuess()
+
+    print('Preparing PromptToSketch Model')
+    pts = PromptToSketch()
 
 @app.route('/prompt_to_sketch', methods=['GET'])
 def prompt_to_sketch():
@@ -14,13 +25,14 @@ def prompt_to_sketch():
     if not prompt:
         return jsonify({"error": "Prompt required"}), 400
     
-    # Generate and return image here
-    
-    return jsonify({"success": ""}), 200
+    img = pts.sketch(prompt) if not TEST_MODE else Image.open('test_imgs/pizza.png')
+    buffer = BytesIO()
+    img.save(buffer, format="PNG")
+    img_str = base64.b64encode(buffer.getvalue()).decode()
+    return jsonify({"success": "Image generated", "image": img_str}), 200
 
 @app.route('/sketch_to_guess', methods=['POST'])
 def sketch_to_guess():
-    print('Recieved Request..')
     if 'file' not in request.files:
         return jsonify({"error": "No file part"}), 400
     
@@ -36,7 +48,7 @@ def sketch_to_guess():
         except Exception as e:
             return jsonify({"error": f"Error opening the image: {str(e)}"}), 400
         
-        guess = stg.guess(image)
+        guess = stg.guess(image) if not TEST_MODE else 'pizza'
 
         return jsonify({"guess": guess}), 200
     else:
